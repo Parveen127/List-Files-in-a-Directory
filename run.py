8ef1eb3c2d579c2e540205762804c9f83d136f2f
M Sajid Mansoori <sajid.mansoori12@gmail.com>
2020-10-01 00:13:25 +0530

    Fixed the issue of creating a text files with list of directory

--- run.py
+++ run.py
@@ -2,11 +2,22 @@ import os
 import glob
 
 name_list = []
-root = 'D:/La Net/Python Django/23. First Clone Project/' #Change this to the dir name you want to list
-# print(lnroot)
+root = 'D:/La Net/Python Django/23. First Clone Project/'
+
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+root_dir = root_dir.replace('\\','/')
+file1 = open("try.txt","a")
+
 for name in glob.glob(root+'*.mp4'):
     name_list.append(name)
 
+#Adding the files to a txt file called try.txt
 for i in name_list:
-    print(i)
+    s=""
+    i=i.replace('.mp4','') #Removing the extension
+    s = i+"\n"
+    file1.write(s)
+    print(i.replace(root_dir,''))
+
+file1.close()
